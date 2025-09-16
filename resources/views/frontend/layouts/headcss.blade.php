49c3627d259316d629e75a2e32acbb53e036733d
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2025-09-16 16:46:47 +0530

    16_09_25_canonical

--- resources/views/frontend/layouts/headcss.blade.php
+++ resources/views/frontend/layouts/headcss.blade.php
@@ -6,6 +6,7 @@
 <meta name="description" content="@yield('description')">
 <meta name="keywords" content="@yield('keywords')">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="canonical" href="{{ url()->current() }}" />
 @yield('meta')
 <title>@yield('title')</title>
 <link rel="shortcut icon" href="{{asset('fronted/assets/mirror-img/fav.jpg')}}" type="image/x-icon">
